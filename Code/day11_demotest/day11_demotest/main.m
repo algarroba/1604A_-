47802c4a4b4187294fd2059beae142a9c99c64aa
algarroba <algarroba@163.com>
2016-08-16 11:50:53 +0800

    test
    test

---
+++ Code/day11_demotest/day11_demotest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  day11_demotest
+//
+//  Created by chris on 16/8/16.
+//  Copyright © 2016年 algarroba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
